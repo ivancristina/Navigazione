eb5f9cf4da3e9636e1fc9fe6d1538b80d886c377
Ivan Cristina <ivancristina5@gmail.com>
2017-09-17 16:53:56 +0200

    Uploading

---
+++ app/src/main/java/com/ivancristina/navigazione/MainActivity.java
@@ -0,0 +1,230 @@
+package com.ivancristina.navigazione;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.*;
+
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+        private ImageView logo;
+        private Button github;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        initialize();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    private void  initialize() {
+        logo = (ImageView) findViewById(R.id.logo);
+        github = (Button) findViewById(R.id.github);
+
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View _v) {
+                showMessage("Non c'è nulla da fare qui, faggot");
+            }
+        });
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View _v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.github.com/ivancristina")));
+            }
+        });
+
+    }
+
+    // created automatically
+    private void showMessage(String _s) {
+        Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+    }
+
+    private int getRandom(int _minValue ,int _maxValue){
+        Random random = new Random();
+        return random.nextInt(_maxValue - _minValue + 1) + _minValue;
+    }
+
+    public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+        ArrayList<Double> _result = new ArrayList<Double>();
+        SparseBooleanArray _arr = _list.getCheckedItemPositions();
+        for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+            if (_arr.valueAt(_iIdx))
+                _result.add((double)_arr.keyAt(_iIdx));
+        }
+        return _result;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_formule) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.content_formule, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.nav_esempi) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.content_esempi, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.nav_calculate) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.content_calculate, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.nav_home) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.content_main, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.formule_coord) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.formule_coord, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.formule_data) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.formule_data, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.formule_distance) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.formule_distance, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.formule_speed) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.formule_speed, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.formule_time) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.formule_time, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        } else if (id == R.id.formule_windtr) {
+
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id. main_container);
+            LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View layout = inflater.inflate(R.layout.formule_windtr, null);
+            mainLayout.removeAllViews();
+            mainLayout.addView(layout);
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
